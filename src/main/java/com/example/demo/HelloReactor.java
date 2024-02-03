67be59a5cc9c8d3bc5915ce31dbd85b9a96830c8
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-02-03 22:51:01 +0900

    걍 FLUX

--- src/main/java/com/example/demo/HelloReactor.java
+++ src/main/java/com/example/demo/HelloReactor.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class HelloReactor {
     public static void main(String[] args) {
         Mono.just("Hello, Reactor")
+                .subscribe(System.out::println);
+
+        Flux<String> flux = Flux.just("Hello", "Reactor");
+        flux
+                .map(String::toLowerCase)
                 .subscribe(System.out::println);
     }
 }
