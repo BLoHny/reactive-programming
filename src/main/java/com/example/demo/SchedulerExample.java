12d4928aebde3acfa1ebd72a608d2d8ae23f16df
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-02-07 11:01:59 +0900

    Scheduler

--- src/main/java/com/example/demo/SchedulerExample.java
+++ src/main/java/com/example/demo/SchedulerExample.java
@@ -6,10 +6,12 @@ import reactor.core.scheduler.Schedulers;
 public class SchedulerExample {
     public static void main(String[] args) {
         Flux.fromArray(new Integer[] {1, 3, 5, 7})
+                .subscribeOn(Schedulers.boundedElastic())
                 .doOnNext(data -> Logger.doOnNext("fromArray", data))
-                .publishOn(Schedulers.parallel())
+                //.publishOn(Schedulers.parallel())
                 .filter(data -> data > 3)
                 .doOnNext(data -> Logger.doOnNext("filter", data))
+                //.publishOn(Schedulers.parallel())
                 .map(data -> data * 10)
                 .doOnNext(data -> Logger.doOnNext("fromArray", data))
                 .subscribe(Logger::onNext);
